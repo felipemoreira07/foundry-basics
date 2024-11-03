23976ece22e2677c013f34bff63d6dd655a8c722
felipemoreira07 <moreiraf242@gmail.com>
2024-11-03 18:57:17 -0300

    feat: create type Pessoa

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    struct Pessoa {
+        uint256 age;
+        string number;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
